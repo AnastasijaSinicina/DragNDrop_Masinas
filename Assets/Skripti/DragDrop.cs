11677b99659fd991e3e0210d7c6304bdfdccff56
Anastasija <sinica.nastja@gmail.com>
2023-05-24 12:18:49 +0300

    Pievienotas masinas, iesakta DragDrop skripta rakstisana

---
+++ Assets/Skripti/DragDrop.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class DragDrop : MonoBehaviour, 
+  IBeginDragHandler, IDragHandler, IEndDragHandler{
+    //uzglabas noradi uz objekti skriptu
+    public Objekti objektuSkripts;
+    //Uzglabas noradi uz katra objekta CanvasGroup
+    private CanvasGroup kanvasGrupa;
+    private RectTransform velkObjRectTransf;
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        kanvasGrupa = GetComponent<CanvasGroup>();
+        velkObjRectTransf= GetComponent<RectTransform>();
+    }
+    
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+    }
+
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+}
