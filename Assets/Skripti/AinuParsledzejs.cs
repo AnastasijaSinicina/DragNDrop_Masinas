49307af04980301ab13ad2e07e65294e58e2fcf3
Anastasija <sinica.nastja@gmail.com>
2023-06-03 20:30:10 +0300

    pogām START un STOP pievienots skripts

---
+++ Assets/Skripti/AinuParsledzejs.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AinuParsledzejs : MonoBehaviour {
+
+	// Use this for initialization
+	public void Apturet()
+	{
+		Application.Quit();
+	}
+	
+	// Update is called once per frame
+	public void uzSpele(){
+		SceneManager.LoadScene("Karte", LoadSceneMode.Single);
+	}
+}
